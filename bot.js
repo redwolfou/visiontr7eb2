c90b3e2b75b1438a79fbecd3f644ab80c81dc4ed
redwolfou <48097704+redwolfou@users.noreply.github.com>
2019-03-03 19:37:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const client = new Discord.Client();
     bot = new Discord.Client({sisableEveryone: true})
     console.log("muuuuuuuuute,");
 bot.on('guildMemberAdd', member => {
-const mohamed= member.guild.channels.get("535461700225204235");
+const mohamed= member.guild.channels.get("551433744527917066");
 if(!mohamed) return;
 if(mohamed) {
-setTimeout(() => mohamed.send(`**:Yoo: WelcomE  `×` To `×` Outside , ! :8631_HyperHeart:  **`), 4000)        
+setTimeout(() => mohamed.send(`**Welcome To Vision. **`), 4000)        
 }
 });
 
